fbb549ef311939bb3048e3d85321ce1d001c8538
Mr. Tchissing <92308356+mrtchissing@users.noreply.github.com>
2021-11-13 02:30:12 +0100

    Update tutorial.py

--- Begginer Project/tutorial.py
+++ Begginer Project/tutorial.py
@@ -1,15 +1,60 @@
 import curses
+import time
 from curses import wrapper
+
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to the Speed Typing Test!")
+    stdscr.addstr("\nPress any key to beggin.")
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+def display_text(stdscr, target, current, wpm=0):
+    stdscr.addstr(target)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
+
+    for i, char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(1)
+        if char != correct_char:
+            color = curses.color_pair(2)
+
+        stdscr.addstr(0, i, char, curses.color_pair(1))
+
+
+def wpm_test(stdscr):
+    target_text = "Hello world this is some text for this app!"
+    current_text = []
+    wpm = 0
+    start_time = time.time()
+
+    while True:
+        time_elapsed = max(time.time () - start_time, 1)
+
+        stdscr.clear()
+        display_text(stdscr, target_text, current_text, wpm)
+        stdscr.refresh()
+
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+
+        if key in ("KEY_BACKSPACE", '\b', '\x7f'):
+            if len(current_text) > 0:
+                current_text.pop()
+        elif len(current_text) < len(target_text):
+            current_text.append(key)
+
+        
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    stdscr.clear()
-    stdscr.addstr(0, 0, "Hello World!")
-    stdscr.refresh()
-    stdscr.getkey()
-    key = stdscr.getkey()
-    print(key)
+
+    start_screen(stdscr)
 
 wrapper(main)
